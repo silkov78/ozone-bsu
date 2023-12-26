8afc2eaecae60bd50aec849fb539e6e305db590c
silkov78 <silkov.piotr78@gmail.com>
2023-12-26 19:40:12 +0300

    extension of devices models

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import News
 from .models import Devices
 from .models import Document
+from .models import TechCharacteristic
+from .models import GalleryImage
+
 
 
 admin.site.site_header = 'ННИЦ МО'
@@ -16,14 +19,32 @@ class NewsAdmin(admin.ModelAdmin):
     save_as = True
 
 
-@admin.register(Devices)
-class DevicesAdmin(admin.ModelAdmin):
-    list_display = ['name', 'time_creation', 'image']
-    readonly_fields = ['last_edit', 'time_creation']
-
-
 @admin.register(Document)
-class DevicesAdmin(admin.ModelAdmin):
+class DocumentAdmin(admin.ModelAdmin):
     list_display = ['name', 'description','document', 'time_creation']
     readonly_fields = ['last_edit', 'time_creation', 'id']
 
+
+
+class DeviceTechCharacteristic(admin.TabularInline):
+    model = TechCharacteristic
+    extra = 1
+    classes = ('collapse', )
+
+
+class DeviceGalleryImage(admin.TabularInline):
+    model = GalleryImage
+    extra = 1
+    classes = ('collapse', )
+
+
+@admin.register(Devices)
+class ProductAdmin(admin.ModelAdmin):
+
+    inlines = [
+        DeviceTechCharacteristic,
+        DeviceGalleryImage
+    ]
+
+    # list_display = ['last_name', 'first_name',  'academic_rank', 'ordering', ]
+    readonly_fields = ('id', 'last_edit', 'time_creation')
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+# class BaseIte(models.Model):
+#     title = 
+
 class News(models.Model):
 
     title = models.CharField(max_length=100, verbose_name='Заголовок')
@@ -48,35 +51,6 @@ class News(models.Model):
         ordering = ['-time_creation']
 
 
-class Devices(models.Model):
-
-    name = models.CharField(max_length=100, verbose_name='Название')
-    description = models.TextField(max_length=255, verbose_name='Описание')
-
-    image = models.ImageField(
-        upload_to='main/devices',
-        verbose_name='Фотография'
-    )
-
-    time_creation = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Добавлен в БД'
-    )
-
-    last_edit = models.DateTimeField(
-        auto_now=True,
-        verbose_name='Последнее изменение'
-    )
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'Прибор'
-        verbose_name_plural = 'Приборы'
-        ordering = ['-time_creation']
-
-
 class Document(models.Model):
 
     name = models.CharField(max_length=150,  verbose_name='Название')
@@ -110,3 +84,105 @@ class Document(models.Model):
         verbose_name = 'Документация'
         verbose_name_plural = 'Документация'
         ordering = ['-time_creation']
+
+
+class Devices(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Название')
+    subtitle = models.CharField(max_length=200, verbose_name='Предназначение')
+
+    description = models.TextField(max_length=1500, verbose_name='Описание')
+
+    main_image = models.ImageField(
+        upload_to='main/devices',
+        verbose_name='Главная фотография'
+    )
+
+    time_creation = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлен в БД'
+    )
+
+    last_edit = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Последнее изменение'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Прибор'
+        verbose_name_plural = 'Приборы'
+        ordering = ['-time_creation']
+
+
+class TechCharacteristic(models.Model):
+    ''' Технические характеристики прибора'''
+
+    device = models.ForeignKey(
+        Devices,
+        on_delete=models.CASCADE,
+        verbose_name='Прибор'
+    )
+
+    characteristic_name = models.CharField(
+        max_length=70,
+        verbose_name='Характеристика'
+    )
+    
+    characteristic_value = models.CharField(
+        max_length=20,
+        verbose_name='Значение'
+    )
+    
+    time_creation = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлен в БД'
+    )
+
+    last_edit = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Последнее изменение'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Характеристика прибора'
+        verbose_name_plural = 'Характеристики прибора'
+        ordering = ['device', '-time_creation']
+
+
+class GalleryImage(models.Model):
+    '''Галлерея фотографий прибора'''
+
+    device = models.ForeignKey(
+        Devices,
+        on_delete=models.CASCADE,
+        verbose_name='Прибор'
+    )
+
+    gallery_image = models.ImageField(
+        upload_to='main/devices_gallery/',
+        verbose_name='Фотография'
+    )
+
+    time_creation = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлен в БД'
+    )
+
+    last_edit = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Последнее изменение'
+    )
+
+    def __str__(self):
+        return f'{self.characteristic_name}'
+
+    class Meta:
+        verbose_name = 'Галерея прибора'
+        verbose_name_plural = 'Галереи приборов'
+        ordering = ['device', '-time_creation']
